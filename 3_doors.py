5638cf363af6a6751e9597d2263ffd513239cf0f
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2023-09-27 12:23:36 +0000

    restructured part 1 with an aim for more reuseability for part 2

--- 3_doors.py
+++ 3_doors.py
@@ -7,37 +7,44 @@ def initialise_doors():
 
 def part1(infomation, part):
     doors = initialise_doors()
-    print(doors)
     round = 1
     switch_wins, stay_wins = 0, 0
-    goat_doors = []
-
+    
+    
     while True:
-
-        print(f"Round #{round}: Door 1 | Door 2 | Door 3")
-        user_choice = int(input("Choose a door: "))
+        choices = [1, 2 ,3]
+        goat_doors = []
+        print(f"\nRound #{round}: Door 1 | Door 2 | Door 3")
+        user_choice = int(input("\nChoose a door: "))
         if user_choice == 0:
             break
+        choices.remove(user_choice)
 
         for i in range(3):
             if doors[i] == 'Goat':
                 goat_doors.append(i+1)
-            else:
-                prize_door = i+1
-        print(goat_doors)
-        print(prize_door)
+
         if goat_doors[0] == user_choice:
-            print(f"Goat is in Door {goat_doors[1]}")
-            
+            print(f"Goat is in Door {goat_doors[1]}")   
+            choices.remove(goat_doors[1])
         else:
             print(f"Goat is in Door {goat_doors[0]}")
+            choices.remove(goat_doors[0])
         
-        switch_choice = input("Stay or Switch? ").strip().lower()
-        if 
+        switch_choice = input("\nStay or Switch? ").strip().lower()
+        if switch_choice == 'switch':
+            
+            user_choice = choices[0]
+        
+        if doors[user_choice-1] == '1 million dollars':
+            print(f"You switched to Door {user_choice + 1} ... You WIN!")
+        else:
+            print(f"You switched to Door {user_choice + 1} ... You lose!")
 
+        round += 1
 
-def finishing_p1(info, stay_wins, switch_wins):
-    print("\n**Summery**")
+def print_summary(info, stay_wins, switch_wins):
+    print("\n**Summary**")
     column_widths = [max(len(str(item)) for item in col) for col in info]
 
     # Determine the number of rows in the table
@@ -68,7 +75,6 @@ def winpercentage(info, wins):
 
 infomation = [["Round"],["Choice"],["Action"],["Outcome"]]
 
-
-which_part = int(input("What part do you want to run"))
+which_part = int(input("\nWhat part do you want to run"))
 
 if which_part == 1: part1(infomation, 1)
