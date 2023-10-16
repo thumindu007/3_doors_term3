4208816538d51d7ebf4b5638cf5a51e3a732aa78
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2023-10-16 13:50:26 +0000

    clean up functions to have less dependency betn each other

--- 3_doors.py
+++ 3_doors.py
@@ -8,8 +8,9 @@ def initialise_doors(part):
     random.shuffle(doors)
     return doors
 
-def game_loop(infomation, part, number_of_rounds, quick_summary):
-    round, switch_wins, stay_wins = 1, 0, 0
+def game_loop(infomation, part, number_of_rounds):
+    round, switch_wins, stay_wins, game_mode = 1, 0, 0, ['User switch','Random switch','Always stay','Always switch','Always switch with 10 doors']
+    print('Running game for mode ', game_mode[0], ' with ', number_of_rounds, ' rounds')
     for _ in range(number_of_rounds):
         doors, choices, goat_doors, round_infomation, switch_choice_list  = initialise_doors(part), [1, 2 ,3], [], [], ['switch', 'stay']
         if part == 5:
@@ -76,19 +77,20 @@ def game_loop(infomation, part, number_of_rounds, quick_summary):
                 round_infomation.append('Lose')
         round += 1
         infomation.append(round_infomation)
-   
+    return stay_wins, switch_wins
 
-# Winning percentage is calculated by amount of times won by switching or staying / total number of wins
+
+# Winning percentage is calculated by amount of times won by switching or staying / total number of rounds
 def print_summary(info, stay_wins, switch_wins, quick_summary, rounds, part):
     for game_round in info[1:]:
         if game_round[2] == "switch" and game_round[3] == "Win":   switch_wins += 1
         elif game_round[2] == "stay" and game_round[3] == "Win":   stay_wins += 1
 
     if quick_summary:
-        quick_sum_list = [["Rounds", "Pr(Win with Switch)", "Pr(Win with Stay)"],[rounds-1, (f"{(switch_wins/(switch_wins+stay_wins))*100}%"), (f'{(stay_wins/(switch_wins+stay_wins))*100}%')]]
+        quick_sum_list = [["Rounds", "Pr(Win with Switch)", "Pr(Win with Stay)"],[rounds, (f"{(switch_wins/(rounds))*100}%"), (f'{(stay_wins/(rounds))*100}%')]]
         quick_summary_print = tabulate(quick_sum_list, tablefmt="fancy_grid", headers="firstrow")
         print(quick_summary_print)
-        if rounds-1 == 1000:
+        if rounds == 1000:
             if part == 2:    output_file = open('part2_random.txt', 'w')
             elif part == 3:  output_file = open('part3_stay.txt', 'w')
             elif part == 4:  output_file = open('part4_switch.txt', 'w')
@@ -100,22 +102,35 @@ def print_summary(info, stay_wins, switch_wins, quick_summary, rounds, part):
         print("\n       **Summary**\nResults Table")
         print(tabulate(info, tablefmt="fancy_grid", headers="firstrow"))
         print(f"\nWins with switching = {switch_wins}\nWins with staying = {stay_wins}")
-        print(f"\n\nPr(Winning with switch) = {(switch_wins/(switch_wins+stay_wins))*100}\nPr(Winning with stay) = {(stay_wins/(switch_wins+stay_wins))*100}\n")
+        print(f"\n\nPr(Winning with switch) = {(switch_wins/(rounds))*100}\nPr(Winning with stay) = {(stay_wins/(rounds))*100}\n")
 
 infomation, part2_rounds = [["Round","Choice","Action","Outcome"]], [50, 100, 1000, 5000, 10000]
-which_part = int(input("\nWhat part do you want to run  "))
+which_part = int(input("\nWhat mode do you want to run the game (1-5) "))
 
-if which_part == 1:   game_loop(infomation, 1, 9999999, False)
+stay_wins, switch_wins, rounds, print_quick_summary, = 0, 0, 1000, True
+if which_part == 1:
+    rounds = 9999999
+    wins = game_loop(infomation, 1, rounds)
+    print_quick_summary = False
 
 elif which_part == 2:
     print("Simulating 50 rounds with random switching.")
-    game_loop(infomation, 2, 50, False)
+    rounds = 50
+    game_loop(infomation, 2, rounds)
+    print_summary(infomation, stay_wins, switch_wins, print_quick_summary, rounds, which_part)
     print("\nSimulating silently...\n\n")
     for i in part2_rounds:
-        game_loop(infomation, 2, i, True)
+        game_loop(infomation, 2, i)
 
-elif which_part == 3:   game_loop(infomation, 3, 1000, True)
+elif which_part == 3:
+    wins = game_loop(infomation, 3, rounds)
 
-elif which_part == 4:   game_loop(infomation, 4, 1000, True)
+elif which_part == 4:
+    wins = game_loop(infomation, 4, rounds)
 
-elif which_part == 5:   game_loop(infomation, 5, 1000, True)
+elif which_part == 5:
+    wins = game_loop(infomation, 5, rounds)
+
+if which_part !=2:
+    stay_wins, switch_wins = wins[0], wins[1]
+    print_summary(infomation, stay_wins, switch_wins, print_quick_summary, rounds, which_part)
